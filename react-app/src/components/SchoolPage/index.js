9c9988fbe48b96eb3b0b55fa849cb32571a5933a
Timothy Wieser <timothyswieser@gmail.com>
2021-09-14 14:03:14 -0400

    adding class on front end completed

--- react-app/src/components/SchoolPage/index.js
+++ react-app/src/components/SchoolPage/index.js
@@ -7,6 +7,23 @@ function SchoolRoute() {
     const [name, setName] = useState("")
     const [location, setLocation] = useState("")
     const [description, setDescription] = useState("")
+    const [classes, setClass] = useState([{ className: "", classDescription: "" }]);
+
+    const handleChange = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...classes];
+        list[index][name] = value;
+        setClass(list);
+    }
+    const handleRemoveClick = (index) => {
+        const list = [...classes];
+        list.splice(index, 1)
+        setClass(list)
+    }
+    const handleAddClick = () => {
+        setClass([...classes, { className: "", classDescription: "" }])
+    }
+    console.log(name)
 
     return (
         <div>
@@ -17,16 +34,32 @@ function SchoolRoute() {
                 <form>
                     <div>
                         <label htmlFor="SchoolName">School Name</label>
-                        <input name="SchoolName" type="text" value={name} onChange={(e) => setName(e.target)}></input>
+                        <input name="SchoolName" type="text" onChange={(e) => setName(e.target)}></input>
                     </div>
                     <div>
                         <label htmlFor="location">Location</label>
-                        <input name="location" type="text" value={location} onChange={(e) => setLocation(e.target)}></input>
+                        <input name="location" type="text" onChange={(e) => setLocation(e.target)}></input>
                     </div>
                     <div>
                         <label htmlFor="description">Description</label>
-                        <input name="description" type="text" value={description} onChange={(e) => setDescription(e.target)}></input>
+                        <input name="description" type="text" onChange={(e) => setDescription(e.target)}></input>
                     </div>
+                    <label>Add A Class</label>
+
+                    {classes.map((x, i) => {
+                        console.log(x)
+                        return (
+                            <div>
+                                <label htmlFor="className">Class Name</label>
+                                <input name="className" type="text" value={x.className} onChange={(e) => handleChange(e, i)}></input>
+                                <label htmlFor="classDescription">Class Ages / Description</label>
+                                <input name="classDescription" type="text" value={x.classDescription} onChange={(e) => handleChange(e, i)}></input>
+                                {classes.length !== 1 && <i class="fas fa-minus" onClick={handleRemoveClick} ></i>}
+                                {classes.length - 1 === i && <i class="fas fa-plus" onClick={handleAddClick}></i>}
+
+                            </div>
+                        )
+                    })}
                 </form>
             </div>
         </div>
