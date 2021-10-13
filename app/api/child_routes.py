ca280597ddd9afce04858e2bc7b5416aded67d0a
Timothy Wieser <timothyswieser@gmail.com>
2021-10-13 16:11:44 -0400

    api routes created

--- app/api/child_routes.py
+++ app/api/child_routes.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, request
+from flask_login import current_user
 from ..models import Child, db
 
 child_routes= Blueprint('children', __name__)
 
 @child_routes.route('/children')
 def get_parent_kids():
-    all_children= Child.query.filter
+    all_children= Child.query.filter(Child.user_id == current_user).all()
+    return {'children':[children.to_dict() for children in all_children]}
+
+
+
